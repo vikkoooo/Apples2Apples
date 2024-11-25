6773b719145fdbfc1b6c237517f46eb21e0e2e56
vikkoooo <vikkolundberg@gmail.com>
2024-11-25 15:28:05 +0100

    remove dead comments

--- src/player/Player.java
+++ src/player/Player.java
@@ -8,34 +8,10 @@ import src.network.PlayerConnection;
 
 public class Player {
 	private int playerID;
-	//private boolean isBot;
-	//private boolean online;
 	private PlayerConnection connection;
 	private ArrayList<Card> hand;
 	private ArrayList<Card> greenApples;
 	private IPlayerStrategy strategy;
-
-	// Old constructor for offline and bots
-	/* 
-	public Player(int playerID, List<Card> initialHand, boolean isBot) {
-		this.playerID = playerID;
-		this.isBot = isBot;
-		this.online = false;
-		this.hand = new ArrayList<>(initialHand);
-		this.greenApples = new ArrayList<>();
-	}
-	*/
-
-	/* 
-	public Player(int playerID, boolean isBot, PlayerConnection connection) {
-		this.playerID = playerID;
-		this.isBot = isBot;
-		this.online = true;
-		this.connection = connection;
-		this.hand = new ArrayList<>();
-		this.greenApples = new ArrayList<>();
-	}
-	*/
 
 	// Constructor for offline and bot players
 	public Player(int playerID, ArrayList<Card> initialHand, boolean isBot) {
@@ -67,52 +43,6 @@ public class Player {
 		this.strategy.setHand(this.hand);
 	}
 
-	/* 
-	public void play(ArrayList<PlayedApple> playedApples) {
-		if (isBot) {
-			// introduce synchronized to fix the race condition, causing the original bug
-			// synchronized makes sure only one thread can modify the shared resource playedApples at a time
-			synchronized (playedApples) {
-				playedApples.add(new PlayedApple(playerID, hand.get(0)));
-			}
-			hand.remove(0);
-		} else if (online) {
-			try {
-				String aPlayedApple = connection.getInput().readLine();
-				synchronized (playedApples) { // sync access to playedApples
-					// Convert the string back to a Card object
-					Card playedCard = new RedApple(aPlayedApple);
-					playedApples.add(new PlayedApple(playerID, playedCard));
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Choose a red apple to play");
-			for (int i = 0; i < hand.size(); i++) {
-				System.out.println("[" + i + "]   " + hand.get(i).getText());
-			}
-			System.out.println("");
-	
-			int choice = 0;
-			try {
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				String input = br.readLine();
-				choice = Integer.parseInt(input);
-			} catch (NumberFormatException e) {
-				System.out.println("That is not a valid option");
-				play(playedApples);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			synchronized (playedApples) { // sync access to playedApples
-				playedApples.add(new PlayedApple(playerID, hand.get(choice)));
-			}
-			hand.remove(choice);
-			System.out.println("Waiting for other players\n");
-		}
-	}
-	*/
 	public void play(ArrayList<PlayedApple> playedApples) {
 		strategy.play(playedApples, playerID);
 	}
@@ -130,18 +60,6 @@ public class Player {
 	public ArrayList<Card> getGreenApples() {
 		return greenApples;
 	}
-
-	/**
-	 * I AM UNSURE IF THIS IS EVER USED???
-	 * 
-	* Adds a green apple card to player's won cards collection.
-	* Called when this player's red apple is chosen by the judge.
-	*/
-	/*
-	public void addGreenApple(Card greenApple) {
-		greenApples.add(greenApple);
-	}
-		 */
 
 	public IClientOutput getOutToClient() {
 		return connection;
