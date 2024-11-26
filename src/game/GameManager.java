6f66f7261225b1aa370eb5df19915d12028b7d42
vikkoooo <vikkolundberg@gmail.com>
2024-11-26 16:18:28 +0100

    remove dead code/comments

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -10,27 +10,11 @@ import src.network.INetworkManager;
 import src.player.Player;
 
 public class GameManager {
-	//private ArrayList<Player> players;
 	private DeckManager deckManager;
 	private IPlayerManager playerManager;
 	private IGameRules gameRules;
 	private INetworkManager networkManager;
-	//private int judge;
 	private ArrayList<PlayedApple> playedApples;
-
-	/* 
-	public GameManager(int numberOfOnlinePlayers) throws Exception {
-		//this.players = new ArrayList<>();
-		this.playerManager = new PlayerManager();
-		this.deckLoader = new DeckLoader();
-		this.shuffler = new Shuffler();
-		this.gameRules = new GameRules();
-		this.deckManager = new DeckManager(deckLoader, shuffler);
-		this.networkManager = new NetworkManager();
-		this.playedApples = new ArrayList<>();
-		setupPlayers(numberOfOnlinePlayers);
-	}
-	*/
 
 	// Constructor with dependency injection
 	public GameManager(DeckManager deckManager, IPlayerManager playerManager, IGameRules gameRules,
@@ -53,32 +37,27 @@ public class GameManager {
 					.collect(Collectors.joining(";"));
 			player.getOutToClient().writeMessage(handString);
 			playerManager.addPlayer(player);
-			//players.add(player);
 			System.out.println("Connected to Player ID: " + i);
 		}
 
 		// Add bots if needed
 		for (int i = numberOfOnlinePlayers; i < Constants.MIN_PLAYERS - 1; i++) {
 			ArrayList<Card> hand = deckManager.dealInitialHand(Constants.INITIAL_HAND_SIZE);
-			//players.add(new Player(i, hand, true));
 			playerManager.addPlayer(new Player(i, hand, true));
 		}
 
 		// Add server player
 		ArrayList<Card> hand = deckManager.dealInitialHand(Constants.INITIAL_HAND_SIZE);
-		//players.add(new Player(players.size(), hand, false));
 		playerManager.addPlayer(new Player(playerManager.getActivePlayers().size(), hand, false));
 	}
 
 	public void startGame() throws Exception {
 		boolean finished = false;
 		playerManager.initializeJudgeIndex();
-		//judge = ThreadLocalRandom.current().nextInt(players.size());
 
 		while (!finished) {
 			playRound();
 			finished = checkWinCondition();
-			//judge = (judge + 1) % playerManager.getActivePlayers().size();
 			playerManager.rotateJudge();
 		}
 	}
@@ -87,7 +66,6 @@ public class GameManager {
 		Player judge = playerManager.getJudge();
 
 		System.out.println("*****************************************************");
-		//if (judge == players.size() - 1) {
 		if (playerManager.getActivePlayers().indexOf(judge) == playerManager.getActivePlayers().size() - 1) {
 			System.out.println("**                 NEW ROUND - JUDGE               **");
 		} else {
@@ -100,7 +78,6 @@ public class GameManager {
 		System.out.println("Green apple: " + greenAppleCard + "\n");
 
 		for (Player player : playerManager.getActivePlayers()) {
-			//if (player != playerManager.getActivePlayers().get(judge)) {
 			if (player != judge) {
 				player.play(playedApples);
 			}
@@ -113,21 +90,13 @@ public class GameManager {
 			System.out.println("[" + i + "] " + playedApples.get(i).redApple);
 		}
 
-		//PlayedApple winningApple = playerManager.getActivePlayers().get(judge).judge(playedApples);
 		PlayedApple winningApple = judge.judge(playedApples);
-		//players.get(winningApple.playerID).getGreenApples().add(greenAppleCard);
 		playerManager.getActivePlayers().get(winningApple.playerID).getGreenApples().add(greenAppleCard);
 
 		System.out.println("Player ID " + winningApple.playerID + " won with: " + winningApple.redApple + "\n");
 
 		playedApples.clear();
 
-		//for (Player player : playerManager.getActivePlayers()) {
-		//	if (player != playerManager.getActivePlayers().get(judge)) {
-		//		Card redCard = deckManager.drawRedApple();
-		//		player.addCard(redCard);
-		//	}
-		//}
 		dealCards();
 	}
 
